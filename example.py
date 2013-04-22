4733ec1d58a20faddf0e6f843689084e15d1e428
Иван <ivan@ivan.gureev>
2013-04-22 09:56:06 +0400

    Пути к примерам

--- example.py
+++ example.py
@@ -1,42 +1,46 @@
 # -*- encoding: utf-8 -*-
 
+import os
+
 from checkers import CheckPhp, CheckJavaScript, CheckCss, CheckHtml, CheckPython, CheckLess
 
 if __name__ == "__main__":
-    content = open('/home/ivan/projects/codestyle/example_code/phpcs.php', 'r').read()
+    example_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'example_code')
+
+    content = open(os.path.join(example_dir, 'phpcs.php'), 'r').read()
     checker_object = CheckPhp()
     checker_object.check(content)
     print len(checker_object.errors_list)
     print checker_object.errors_list[1].__dict__
 
-    content = open('/home/ivan/projects/codestyle/example_code/javascript.js', 'r').read()
+    content = open(os.path.join(example_dir, 'javascript.js'), 'r').read()
     checker_object = CheckJavaScript()
     checker_object.check(content)
     print len(checker_object.errors_list)
     print checker_object.errors_list[1].__dict__
 
-    content = open('/home/ivan/projects/codestyle/example_code/csslint.css', 'r').read()
+    content = open(os.path.join(example_dir, 'csslint.css'), 'r').read()
     checker_object = CheckCss()
     checker_object.check(content)
     print len(checker_object.errors_list)
     print checker_object.errors_list[1].__dict__
     print checker_object.errors_list[-1].__dict__
 
-    content = open('/home/ivan/projects/codestyle/example_code/tidy.html', 'r').read()
+    content = open(os.path.join(example_dir, 'tidy.html'), 'r').read()
     checker_object = CheckHtml()
     checker_object.check(content)
     print len(checker_object.errors_list)
     print checker_object.errors_list[1].__dict__
     print checker_object.errors_list[-1].__dict__
 
-    content = open('/home/ivan/projects/codestyle/example_code/python.py', 'r').read()
+    content = open(os.path.join(example_dir, 'python.py'), 'r').read()
     checker_object = CheckPython()
     checker_object.check(content)
     print len(checker_object.errors_list)
     print checker_object.errors_list[0].__dict__
     print checker_object.errors_list[-1].__dict__
 
-    content = open('/home/ivan/projects/codestyle/example_code/less/style.less', 'r').read()
+    content = open(os.path.join(example_dir, 'less/style.less'), 'r').read()
     checker_object = CheckLess()
     checker_object.check(content)
     print len(checker_object.errors_list)
